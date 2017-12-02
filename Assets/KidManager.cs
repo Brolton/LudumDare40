80ab69c03cf8ac01a7682f4de6f0d10caebfc19c
Jaroslav Vozár <jarkovozar@gmail.com>
2017-12-02 14:00:19 +0100

    Added Kidmanager - spawns kids at random location after fixed interval of time

---
+++ Assets/KidManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidManager : MonoBehaviour
+{
+    public GameObject kid;
+    public float spawnTime = 7f;    // Spawn time in seconds - NEEDS TO BE ADJUSTED
+    public float timeOfFirstSpawn = 0;
+    public float offsetFromBorders = 5;
+
+    public float XSizeOfPlayArea;
+    public float ZSizeOfPlayArea;
+
+    void Start ()
+    {
+        InvokeRepeating("Spawn", timeOfFirstSpawn, spawnTime);
+	}
+
+
+    void Spawn()
+    {
+        float randomXCoordinate = Random.Range(offsetFromBorders, XSizeOfPlayArea - offsetFromBorders);
+        float randomZCoordinate = Random.Range(offsetFromBorders, ZSizeOfPlayArea - offsetFromBorders);
+
+        Vector3 spawnPosition = new Vector3(randomXCoordinate, 0, randomZCoordinate);
+
+        Instantiate(kid, spawnPosition, Quaternion.identity);
+    }
+}
