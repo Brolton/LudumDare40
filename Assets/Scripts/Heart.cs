ad42c66f9da818242f85d4c60f10557eb82fb5a9
RSDuck <Kemal-Afzal@outlook.de>
2017-12-04 00:06:49 +0100

    more realistic heart animation

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -3,58 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Heart : MonoBehaviour 
+public class Heart : MonoBehaviour
 {
-	enum ScaleState {
-		INCR,
-		DECR
-	}
+    enum ScaleState
+    {
+        INCR,
+        DECR
+    }
 
-	public Image heartImage;
+    public Image heartImage;
 
-	float currentScale = 1f;
-	float curMaxScale;
-	float curMinScale;
-	float curSpeed; // scale per second
-	ScaleState curScaleState = ScaleState.INCR;
+	public float secondaryMaxScaleFactor = 0.8f;
 
-	public float normalMaxScale = 1.1f;
-	public float normalMinScale = 0.9f;
-	public float normalSpeed = 0.5f; // scale per second
 
-	public float panicMaxScale = 1.2f;
-	public float panicMinScale = 0.8f;
-	public float panicSpeed = 1.0f; // scale per second
+    float currentScale = 1f;
+    float curMaxScale;
+    float primaryMaxScale;
+    float secondaryMaxScale;
+    bool secondaryBeat;
+    float curMinScale;
+    float curSpeed; // scale per second
+    ScaleState curScaleState = ScaleState.INCR;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (curScaleState == ScaleState.INCR) {
-			currentScale += curSpeed * Time.deltaTime;
-			if (currentScale >= curMaxScale) {
-				currentScale = curMaxScale;
-				curScaleState = ScaleState.DECR;
-			}
-		}
-		else if (curScaleState == ScaleState.DECR) {
-			currentScale -= curSpeed * Time.deltaTime;
-			if (currentScale <= curMinScale) {
-				currentScale = curMinScale;
-				curScaleState = ScaleState.INCR;
-			}
-		}
+    public float normalMaxScale = 1.1f;
+    public float normalMinScale = 0.9f;
+    public float normalSpeed = 0.5f; // scale per second
 
-		heartImage.GetComponent<RectTransform> ().localScale = new Vector3 (currentScale, currentScale);
-	}
+    public float panicMaxScale = 1.2f;
+    public float panicMinScale = 0.8f;
+    public float panicSpeed = 1.0f; // scale per second
 
-	public void SetBpm(float bpmPercent) 
-	{
-		curMaxScale = normalMaxScale + (panicMaxScale - normalMaxScale) * bpmPercent / 100f;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curScaleState == ScaleState.INCR)
+        {
+            currentScale += curSpeed * Time.deltaTime;
+            if (currentScale >= curMaxScale)
+            {
+                currentScale = curMaxScale;
+                curScaleState = ScaleState.DECR;
+                secondaryBeat ^= true;
+                curMaxScale = secondaryBeat ? secondaryMaxScale : primaryMaxScale;
+            }
+        }
+        else if (curScaleState == ScaleState.DECR)
+        {
+            currentScale -= curSpeed * Time.deltaTime;
+            if (currentScale <= curMinScale)
+            {
+                currentScale = curMinScale;
+                curScaleState = ScaleState.INCR;
+            }
+        }
+
+        heartImage.GetComponent<RectTransform>().localScale = new Vector3(currentScale, currentScale);
+    }
+
+    public void SetBpm(float bpmPercent)
+    {
 		curMinScale = normalMinScale + (panicMinScale - normalMinScale) * bpmPercent / 100f;
-		curSpeed = normalSpeed + (panicSpeed - normalSpeed) * bpmPercent / 100f;
-	}
+        curSpeed = normalSpeed + (panicSpeed - normalSpeed) * bpmPercent / 100f;
+
+		primaryMaxScale = normalMaxScale + (panicMaxScale - normalMaxScale) * bpmPercent / 100f;
+		
+		secondaryMaxScale = Mathf.Max(primaryMaxScale * secondaryMaxScaleFactor, curMinScale);
+    }
 }
