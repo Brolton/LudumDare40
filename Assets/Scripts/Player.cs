f4e23d01185bfbb4b65571ea085a496753972722
Jaroslav Vozár <jarkovozar@gmail.com>
2017-12-03 14:52:52 +0100

    Glow animation (firefly picked up) applied

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,13 @@ public class Player : MonoBehaviour
     public float speed = normalSpeed;
     public Rigidbody2D rb2D;
     public float heartbeat = 0;     // value between 0 and 100
-    public float lightVicinity;
+    public float lightVicinity = 5;     // TO BE CHANGED
 
     public GameManager gameManager;
 
     public FireflyManager fireflyManager;
 
-    public const float heartbeatDecreasePerPickedUpFirefly = 25f;     // TO BE CHANGED (placeholder value)
+    public const float heartbeatDecreasePerPickedUpFirefly = 25f;
     public const float lengthOfDecreasingHeartbeatAfterPickedUpFirefly = 5f;
     public const float decreaseOfHeartbeatInOneSecond = heartbeatDecreasePerPickedUpFirefly / lengthOfDecreasingHeartbeatAfterPickedUpFirefly;
     public const float maxHeartbeatForRunning = 80f;
@@ -21,6 +21,8 @@ public class Player : MonoBehaviour
     public const float runningSpeed = 2f;
     public const float normalHeartbeatIncrease = 1f;
     public const float runningHeartbeatIncrease = 4f;
+    public const float pickedUpFireflyCountdownSet = 1.5f;
+    float pickedUpFireflyCountdown = 0;
     public const string enemyTag = "enemy";
 
     private float continuoslyDecreaseHeartbeat;
@@ -28,10 +30,6 @@ public class Player : MonoBehaviour
 	public SpriteRenderer sprite;
 
     public Player instance;
-
-    public Sprite run;
-    public Sprite idle;
-    public Sprite walk;
 
     public RuntimeAnimatorController runAnim;
     public RuntimeAnimatorController idleAnim;
@@ -51,6 +49,14 @@ public class Player : MonoBehaviour
         var horizontalMovementRaw = Input.GetAxisRaw("Horizontal");
         var vericalMovementRaw = Input.GetAxisRaw("Vertical");
 
+        // Player recently picked up firefly - play glow animation and stay in one place
+        if (pickedUpFireflyCountdown > 0)
+        {
+            animator.runtimeAnimatorController = glowAnim;
+            pickedUpFireflyCountdown -= Time.deltaTime;
+            return;
+        }
+
         // Is running
         if (Input.GetKey(KeyCode.LeftShift) && heartbeat < maxHeartbeatForRunning)
         {
@@ -67,6 +73,7 @@ public class Player : MonoBehaviour
 		if (horizontalMovementRaw != 0)
 			sprite.flipX = (horizontalMovementRaw < 0);     // rotation to direction of movement
 
+        //Selection of animation
         if (horizontalMovementRaw == 0 && vericalMovementRaw == 0)
         {
             animator.runtimeAnimatorController = idleAnim;
@@ -84,7 +91,7 @@ public class Player : MonoBehaviour
         }
 
         
-        // Clipping
+        // Clipping of heartbeat value
         if (heartbeat < 0f)
         {
             heartbeat = 0f;
@@ -116,6 +123,7 @@ public class Player : MonoBehaviour
     public void FireflyPickedUp()
     {
         DestroyEnemiesInsideLightVicinity();
+        pickedUpFireflyCountdown = pickedUpFireflyCountdownSet;
 
         instance.continuoslyDecreaseHeartbeat = heartbeatDecreasePerPickedUpFirefly;            
     }
