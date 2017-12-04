254cf7bea3273bef53cef89f56af9068edab9733
hasuprobe <hasuprobe@gmail.com>
2017-12-04 23:25:17 +0100

    Death stops music correctly

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour
 		AkSoundEngine.PostEvent ("PLAY_death", gameObject);					// Wwise play death sound
 		AkSoundEngine.PostEvent ("STOP_heartbeat", gameObject);				// Wwise stop heartbeat sound
 		AkSoundEngine.PostEvent ("STOP_lantern", gameObject);				// Wwise stop lantern sound
-		AkSoundEngine.PostEvent ("STOP_heartsAndHeartbeats", gameObject);	// Wwise stop MUSIC
+		AkSoundEngine.PostEvent ("STOP_heartsAndFireflies", gameObject);	// Wwise stop MUSIC
     }
 
     public void PickUpFirefly() { score += FireflyDtScore; }
