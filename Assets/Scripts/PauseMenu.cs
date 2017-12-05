7878560004ab8a2ee40cae1f384dd7655910664a
YourBoyConnor <33465131+YourBoyConnor@users.noreply.github.com>
2017-12-04 20:53:52 -0500

    pause script

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject title;
+    public GameObject resume;
+    public GameObject resumetext;
+    public GameObject bg;
+    bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+
+        Time.timeScale = 1;
+        title.SetActive(false);
+        resume.SetActive(false);
+        resumetext.SetActive(false);
+        bg.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (paused == true)
+        {
+            
+            Time.timeScale = 0;
+            title.SetActive(true);
+            resume.SetActive(true);
+            resumetext.SetActive(true);
+            bg.SetActive(true);
+
+        } else
+        {
+            
+            Time.timeScale = 1;
+            title.SetActive(false);
+            resume.SetActive(false);
+            resumetext.SetActive(false);
+            bg.SetActive(false);
+
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+
+            paused = true;
+
+        }
+
+	}
+
+    public void Resume()
+    {
+
+        paused = false;
+
+    }
+
+}
